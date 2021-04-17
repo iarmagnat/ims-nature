27833f278b8635b20dc1b74547c40c0ab437b093
ivan.armagnat <ivan.armagnat_ext@michelin.com>
2021-04-18 00:05:57 +0200

    An evening's worth of work

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html', ma_super_variable="ponky")
+    return render_template('home.html')
 
 
 @app.route('/huppe')
 def huppe():
     with open('content/Oiseaux/huppe.json') as json_file:
         data = json.load(json_file)
-        return render_template('huppe.html', data=data)
+        return render_template('species.html', data=data)
 
 
 @app.route('/pwet')
