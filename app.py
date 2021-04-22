24248d72ded5c4166c3e3feee9e2ce63bf3f00bc
ivan.armagnat <ivan.armagnat_ext@michelin.com>
2021-04-23 00:26:30 +0200

    Add category page

--- app.py
+++ app.py
@@ -17,6 +17,15 @@ def huppe():
         return render_template('species.html', data=data)
 
 
+@app.route('/oiseaux')
+def oiseaux():
+    with open('content/Oiseaux/index.json') as json_file:
+        data = json.load(json_file)
+        sorted_children = sorted(data["children"], key=lambda species: species["vernacular"])
+        data["sorted_children"] = sorted_children
+        return render_template('category.html', data=data)
+
+
 @app.route('/pwet')
 def home2():
     return 'pwet'
